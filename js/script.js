42e0f9ca004812adb6b3e02f7734c9c3518cbbe5
Anne SK <92764640+wesiptea@users.noreply.github.com>
2022-05-01 11:12:15 -0700

    Updates to js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const guessedLettersList = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
-// This button is referred to by its class of "guess" to distiguish it from the second button below.
+// Buttons have been selected w class names, "guess" and "play-again hide," to distiguish them from each other.
 const playerGuess = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
@@ -9,27 +9,68 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again hide");
 
 const word = "magnolia";
+const guessedLetters = [];
+// This empty array will contain all of the letters of player guesses
 
 // Display the dots as placeholders for word letters
 const placeholders = function (word) {
     const placeholdersForWord = [];
-    // This empty array allows each letter placeholder of the word to be an item in the array.
+    // This empty array allows each letter of a selected word to be converted into an array.
     for (const letter of word) {
-        // declared a var called 'letter' - created for this loop = for converting every letter into a separate item
+        // Declared a var called 'letter' to loop through letters of words = this is to ensure that every letter is a separate item.
         console.log(letter);
+
         placeholdersForWord.push("●");
-        // The above for...of loop enables the code to create a placeholder for each letter of target word 
+        // This allows a dot to be pushed out for each letter of the selected word.
     }
     wordInProgress.innerText = placeholdersForWord.join("");
-    // If you leave the .join() empty - commas will separate array items. If you include "" then commas will not be included. ex: console.log(alphabet.join("")); // abc
-
+    // If you leave the .join() empty - commas will separate array items. If you include "" w/in the method, commas will not be included. 
+    // ex: console.log(alphabet.join("")); // abc
 }
 placeholders(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    // this code above prevents the page from reloading when button is clicked and reset everything
-    const guessedLetter = playerGuess.value;
-    console.log(guessedLetter);
+    // prevents the page from reloading when button is clicked and reseting everything on screen (clearing guessed letters, etc)
+    
+    message.innerText = "";
+
+    // Below: captures the value of the input. Logs out the value of the variable capturing the input. Then, empties the value of the input. 
+    // .value is code which allows us to "capture" whatever is entered (playerGuess) into a form.
+    const guess = playerGuess.value;
+   
+    const validGuess = validInput(guess);
+
+    if (validGuess) {
+        makeGuess(guess);            
+    }
+
     playerGuess.value = "";
+    // This empty string clears the box so the next letter can be guessed.
 });
+
+const validInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    // This code makes sure that only letters are accepted as input into form.
+    if (input.length === 0) {
+        message.innerText = "Whoops, you forgot to enter a letter!";
+    } else if (input.length > 1) {
+        message.innerText = "Please enter just 1 letter.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please a letter (a-z or A-Z).";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(guessedLetter) {
+    guessedLetter = guessedLetter.toUpperCase();
+    // Change all letters to uppercase to avoid issues with case sensitivity
+    console.log(guessedLetter);
+    
+    if (validInput.includes(guessedLetters)) {
+        message.innerText = "You have already guessed that letter, try a new letter."
+    } else {
+        guessedLetters.push("");
+    }
+};
