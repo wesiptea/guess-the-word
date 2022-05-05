df55b362b804261410c10e7b7b7ec2c6c7e794df
Anne SK <92764640+wesiptea@users.noreply.github.com>
2022-05-04 18:01:17 -0700

    Finished step two, adding letter array

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const guessedLetters = [];
 // This empty array will contain all of the letters of player guesses
 
 // Display the dots as placeholders for word letters
-const placeholders = function (word) {
+const placeholders = function(word) {
     const placeholdersForWord = [];
     // This empty array allows each letter of a selected word to be converted into an array.
     for (const letter of word) {
@@ -29,11 +29,12 @@ const placeholders = function (word) {
 }
 placeholders(word);
 
-guessButton.addEventListener("click", function (e) {
+guessButton.addEventListener("click", function(e) {
     e.preventDefault();
     // prevents the page from reloading when button is clicked and reseting everything on screen (clearing guessed letters, etc)
     
     message.innerText = "";
+    // Empty message paragraph
 
     // Below: captures the value of the input. Logs out the value of the variable capturing the input. Then, empties the value of the input. 
     // .value is code which allows us to "capture" whatever is entered (playerGuess) into a form.
@@ -44,12 +45,11 @@ guessButton.addEventListener("click", function (e) {
     if (validGuess) {
         makeGuess(guess);            
     }
-
     playerGuess.value = "";
     // This empty string clears the box so the next letter can be guessed.
 });
 
-const validInput = function (input) {
+const validInput = function(input) {
     const acceptedLetter = /[a-zA-Z]/;
     // This code makes sure that only letters are accepted as input into form.
     if (input.length === 0) {
@@ -67,11 +67,11 @@ const makeGuess = function(guessedLetter) {
     guessedLetter = guessedLetter.toUpperCase();
     // Change all letters to uppercase to avoid issues with case sensitivity
     
-    if (validInput.includes(guessedLettersList)) {
+    if (guessedLetters.includes(guessedLetter)) {
         message.innerText = "You have already guessed that letter, try a new letter."
     } else {
-        validInput.push(guessedLetters);
+        guessedLetters.push(guessedLetter);
     }
     console.log(guessedLetters);
 };
- 
+
